ccf4b358cfcd0a524b5ce55f8994d47e009e346d
riley <rileyduke@gmail.com>
2023-04-26 15:10:50 +0900

    add binary search logic

--- etl/get_data.js
+++ etl/get_data.js
@@ -1,6 +1,6 @@
-const https = require('https');
-const fs = require('fs');
-var path = require('path');
+const https = require('https')
+const fs = require('fs')
+var path = require('path')
 
 const helper = require('./helper.js')
 
@@ -9,21 +9,48 @@ var file_directory = './output/'
 var file_path = './fallback.json'
 
 function getRange(fromDate, toDate) {
+    console.log("from: " + fromDate)
+    console.log("to: " + toDate)
+    let from = new Date(fromDate)
+    let to = new Date(toDate)
+    console.log(helper.differenceInDays(from, to))
+    if (helper.differenceInDays(from, to) < 1) {
+        console.log("No difference in days")
+        return
+    }
+
     let url = new URL(api_url)
     url.searchParams.append('fromDate', fromDate)
     url.searchParams.append('toDate', toDate)
-
-    let from = new Date(fromDate)
-    let to = new Date(toDate)
     
+    var data = []
     https.get(url, function (res) { 
+        // too many arguments status code
         if (res.statusCode == 400) {
             // binary search until we can get some results
-
+            let diff = helper.differenceInDays(from, to)
+            let midpoint = helper.addDays(from, diff / 2)
+            let midpoint1 = helper.addDays(from, (diff / 2) + 1)
+            getRange(fromDate, helper.dateToString(midpoint))
+            getRange(helper.dateToString(midpoint1), toDate)
             return
         }
-        // write the results
 
+        // unknown status code
+        if (res.statusCode != 200){
+            return
+        }
+
+        // successfully got a response
+        res.on('data', function(chunk) {
+            data.push(chunk);
+        }).on('end', function() {
+            var events = JSON.parse(Buffer.concat(data));
+            events.data.forEach(x => {
+                fs.appendFileSync(file_path, "\n")
+                fs.appendFileSync(file_path, helper.recordToCsv(x))
+            });
+        });
     });
 }
 
@@ -34,4 +61,4 @@ const now = new Date().getTime();
 file_path = path.join(file_directory, now + ".json")
 
 fs.writeFileSync(file_path, 'Date,Amount,Description');
-getRange('2022-03-01', '2022-03-05')
+getRange('2022-01-01', '2023-03-05')
--- etl/helper.js
+++ etl/helper.js
@@ -3,6 +3,16 @@ module.exports = {
         return d.toISOString().split('T')[0]
     },
     recordToCsv: function(r) {
-        return ""
+        return `${r.date}, ${r.amount.replace(/,/g, '')}, '${r.description}'`
+    },
+    differenceInDays: function(from, to) {
+        let difference = to.getTime() - from.getTime();
+        let totalDays = Math.ceil(difference / (1000 * 3600 * 24));
+        return totalDays;
+    },
+    addDays: function(d, days) {
+        let newd = new Date(d.valueOf())
+        newd.setDate(newd.getDate() + days);
+        return newd;
     },
 }
